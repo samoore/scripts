184313c5eed3c3dd76d21bbdb0e6f93be67fdda8
Jonny Williams <jonny.williams@niwa.co.nz>
2018-05-18 01:30:08 +0000

    adding 34150

--- process-raw-data-for-auto-assess.py
+++ process-raw-data-for-auto-assess.py
@@ -31,30 +31,36 @@ import mule
 
 #Let's read in some raw model data...
 
-stashcode=['m01s30i201','m01s30i204','m01s30i205']
+stashcode=['m01s30i201','m01s30i204','m01s30i205','m01s34i150']
 
 stash = iris.AttributeConstraint(STASH=stashcode[0])
 
-var30201 = iris.load('/home/williamsjh/cylc-run/u-ai955/share/data/History_Data/ai955a.pg19880901',stash)
+var30201 = iris.load_cube('/home/williamsjh/cylc-run/u-ai955/share/data/History_Data/ai955a.pg19880901',stash)
 
 stash = iris.AttributeConstraint(STASH=stashcode[1])
 
-var30204 = iris.load('/home/williamsjh/cylc-run/u-ai955/share/data/History_Data/ai955a.pg19880901',stash)
+var30204 = iris.load_cube('/home/williamsjh/cylc-run/u-ai955/share/data/History_Data/ai955a.pg19880901',stash)
 
 stash = iris.AttributeConstraint(STASH=stashcode[2])
 
-var30205 = iris.load('/home/williamsjh/cylc-run/u-ai955/share/data/History_Data/ai955a.pg19880901',stash)
+var30205 = iris.load_cube('/home/williamsjh/cylc-run/u-ai955/share/data/History_Data/ai955a.pg19880901',stash)
 
-#Now we want the zonal mean of this...
+stash = iris.AttributeConstraint(STASH=stashcode[3])
 
-var30201_zm = var30201[0].collapsed('longitude',iris.analysis.MEAN)
-var30204_zm = var30204[0].collapsed('longitude',iris.analysis.MEAN)
-var30205_zm = var30205[0].collapsed('longitude',iris.analysis.MEAN)
+var34150 = iris.load_cube('/home/williamsjh/cylc-run/u-ai955/share/data/History_Data/ai955a.pg19880901',stash)
+
+#Now we want to calculate some zonal means...
+
+var30201_zm = var30201.collapsed('longitude',iris.analysis.MEAN)
+var30204_zm = var30204.collapsed('longitude',iris.analysis.MEAN)
+var30205_zm = var30205.collapsed('longitude',iris.analysis.MEAN)
+
+#let's print the variables to the screen
 
 print var30201_zm
 print var30204_zm
 print var30205_zm
-
+print var34150
 
 
 
